d0c53dcd79dfd844046963868fb8dffe6da1577f
MalluBotsYT <106857814+MalluBotsYT@users.noreply.github.com>
2022-07-12 19:18:57 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -40,7 +40,8 @@ class mr(object):
     DEV_TXT = """
 <b><u>ᴅᴇᴠᴇʟᴏᴘᴇʀs</b></u> 
 
-» ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/MalluHubYT>ᴍᴀʟʟᴜʜᴜʙʏᴛ</a>
+» ᴜᴘᴅᴀᴛᴇs : <a href=https://t.me/MalluBotsYT>ᴍᴀʟʟᴜʙᴏᴛsʏᴛ</a>
+» sᴜᴘᴘᴏʀᴛ : <a href=https://t.me/MalluBotsGP>ᴍᴀʟʟᴜʙᴏᴛsɢᴘ
 
 • ❣️ <a href=https://t.me/Crude_X>ᴄʀᴜᴅᴇ_x</a>
 • ❣️ <a href=https://t.me/Bea_s_t2>ʙᴇᴀsᴛ</a>
