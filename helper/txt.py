0dcf22cdaace1ba4244965266f8833f25d8813b4
MalluBotsYT <106857814+MalluBotsYT@users.noreply.github.com>
2022-07-12 18:46:28 +0530

    Add files via upload

---
+++ helper/txt.py
@@ -0,0 +1,47 @@
+class mr(object):
+    PROGRESS_BAR = """\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣⪼ 🗂️ : {1} | {2}
+┣⪼ ⏳️ : {0}%
+┣⪼ 🚀 : {3}/s
+┣⪼ ⏱️ : {4}
+╰━━━━━━━━━━━━━━━➣ """
+
+    ABOUT_TXT = """
+╭────────────⍟
+├🤖 ᴍʏ ɴᴀᴍᴇ : {}
+├👑 ᴏᴡɴᴇʀ : <a href=https://t.me/Crude_X>ᴄʀᴜᴅᴇ_x</a> 
+├👨‍💻 ᴘʀᴏɢʀᴀᴍᴇʀ : <a href=https://github.com/PbAjay>ᴘʙᴀᴊᴀʏ</a>
+├📕 ʟɪʙʀᴀʀʏ : <a href=https://github.com/pyrogram>ᴘʏʀᴏɢʀᴀᴍ</a>
+├✏️ ʟᴀɴɢᴜᴀɢᴇ : <a href=https://www.python.org>ᴘʏᴛʜᴏɴ 3</a>
+├💾 ᴅᴀᴛᴀ ʙᴀsᴇ : <a href=https://cloud.mongodb.com>ᴍᴀɴɢᴏ ᴅʙ</a>
+├🌀 ᴍʏ sᴇʀᴠᴇʀ : <a href=https://dashboard.heroku.com>ʜᴇʀᴏᴋᴜ</a>
+├📊 ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ3.6.8 [ ᴍᴀᴊᴏʀ ]              
+╰───────────────⍟
+                                """
+    HELP_TXT = """
+🌌 <b><u>ʜᴏᴡ ᴛᴏ sᴇᴛ ᴛʜᴜᴍʙɴᴀɪʟ</u></b>
+  
+•> /start : ᴀ ʙᴏᴛ ᴀɴᴅ sᴇɴᴅ ᴀɴʏ ᴘɪᴄᴛᴜʀᴇ ᴛᴏ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ sᴇᴛ ᴛʜᴜᴍʙɴᴀɪʟ
+•> /delthumb : ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴀɴᴅ ᴅᴇʟᴇᴛᴇ ʏᴏᴜʀ ᴏʟᴅ ᴛʜᴜᴍʙɴᴀɪʟ
+•> /viewthumb : ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴠɪᴇᴡ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ᴛʜᴜᴍʙɴᴀɪʟ
+
+📑 <b><u>ʜᴏᴡ ᴛᴏ sᴇᴛ ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ</u></b>
+•> /set_caption : sᴇᴛ ᴀ ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ
+•> /see_caption : sᴇᴇ ʏᴏᴜʀ ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ
+•> /del_caption : ᴅᴇʟᴇᴛᴇ ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ
+
+✏️ <b><u>ʜᴏᴡ ᴛᴏ ʀᴇɴᴀᴍᴇ ᴀ ғɪʟᴇ</u></b>
+•> sᴇɴᴅ ᴀɴʏ ғɪʟᴇ ᴀɴᴅ ᴄʟɪᴄᴋ ʀᴇɴᴀᴍᴇ ᴏᴘᴛɪᴏɴ ᴀɴᴅ ᴛʏᴘᴇ ɴᴇᴡ ғɪʟᴇ ɴᴀᴍᴇ ᴀɴᴅ \n sᴇɴᴅ sᴇʟᴇᴄᴛ [ ᴅᴏᴄᴜᴍᴇɴᴛ, ᴠɪᴅᴇᴏ, ᴀᴜᴅɪᴏ ]👈 ᴄʜᴏɪᴄᴇ ᴛʜɪs.
+ℹ️ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ ᴄᴏɴᴛᴀᴄᴛ :- <a href=https://t.me/Crude_X>ᴏᴡɴᴇʀ</a>
+"""
+
+#⚠️ don't remove our credits 🙏😢😢
+    DEV_TXT = """
+<b><u>ᴅᴇᴠᴇʟᴏᴘᴇʀs</b></u> 
+
+» ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/MalluHubYT>ᴍᴀʟʟᴜʜᴜʙʏᴛ</a>
+
+• ❣️ <a href=https://t.me/Crude_X>ᴄʀᴜᴅᴇ_x</a>
+• ❣️ <a href=https://t.me/Bea_s_t2>ʙᴇᴀsᴛ</a>
+"""
